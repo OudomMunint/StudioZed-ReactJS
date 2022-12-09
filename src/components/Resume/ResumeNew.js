59e1805c7f9a0b970772989a41dc0f31c6150b99
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2022-12-09 16:36:53 +1100

    Fixed cannot find pdf error

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+/* import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
@@ -57,3 +57,4 @@ function ResumeNew() {
 }
 
 export default ResumeNew;
+ */
