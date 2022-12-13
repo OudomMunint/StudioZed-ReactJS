cf398d4eb2100a10fb0138dc22f219466686cb7f
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2022-12-14 09:38:20 +1100

    Font size change, footer content edit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          {/* <h3>Designed and Developed by Soumyajit Behera</h3> */}
+          {/* <h3>Powered By ReactJs, NodeJs & ExpressJs</h3> */}
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} Studio Zed</h3>
