cb11c4c0a8dc898be010d79483170bef4bc1a901
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2022-12-26 02:31:19 +1100

    aboutUs content update

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -124,7 +124,7 @@ function Home2() {
               <br />
               <br />Meet <b><a target="_blank" href="https://www.linkedin.com/in/carl-morgan-7066863b/" style={{textDecoration:"none", color:"white"}} className="purple">Carl, </a></b>
               <i>
-              Associate Lecturer at University of Newcastle & Co-Founder at Studio Zed.
+              Associate Lecturer at University of Newcastle & Co-Founder of Zookraft and Studio Zed.
               </i>
               <br />
               {/* <br /> */}
@@ -156,48 +156,42 @@ function Home2() {
           </Col>
         </Row>
       </Container>
-      {/* carl section End*/}
+{/* carl section End*/}
 
-      {/* Simone section */}
+{/* Simone section */}
       <Container>
         <Row>
           <Col md={8} className="home-about-description">
-          
-          <h1 className="project-heading" style={{textAlign: "right"}}>
-          Meet<strong className="purple"> Out Team! </strong>
-        </h1>
             <p className="home-about-body" style={{width: "500px", textAlign: "left", fontSize: "2.2em"}}>
               <br />
-              <br />Meet <b><a target="_blank" href="https://www.linkedin.com/in/carl-morgan-7066863b/" style={{textDecoration:"none", color:"white"}} className="purple">Carl, </a></b>
-              <i>
-              Associate Lecturer at University of Newcastle & Co-Founder at Studio Zed.
-              </i>
-              <br />
+              <br />Meet <b><a target="_blank" href="https://www.linkedin.com/in/simoneo/" style={{textDecoration:"none", color:"white"}} className="purple">Simone, </a></b>
+              Deputy Head of School, Research Training - School of Humanities, Creative Industries, Social Sciences at University of Newcastle and founder at Studio Zed.
+              {/* <br />
               {/* <br /> */}
-              He is also a very talented &nbsp;
+              &nbsp;She is also an experienced&nbsp;
               <i>
-                <b className="purple">llustrator</b> and{" "}
-                <b className="purple">Designer.</b>
+                <b className="purple">UI UX Designer, Art Director</b> and{" "}
+                <b className="purple">Creative Lead.</b>
               </i>
               <br />
               <br />
-              Reach out to carl on
-              <a target="_blank " href="https://www.instagram.com/zookraft/?hl=en">
+              Reach out to Simone on
+              {/* <a target="_blank " href="https://www.instagram.com/zookraft/?hl=en">
                 <b className="purple">
                   {" "}
                   IG
                 </b>
-              </a>
-              &nbsp;or
-              <a target="_blank " href="https://www.instagram.com/zookraft/?hl=en">
+              </a> */}
+              &nbsp;
+              <a target="_blank " href="https://www.linkedin.com/in/simoneo/">
                 <b className="purple"> linkedin</b>
                 </a>
             </p>
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={carl} className="img-fluid" alt="avatar" style={{ minHeight: "450px"}}/>
-              <a target="_blank" href="https://www.linkedin.com/in/carl-morgan-7066863b/"><figcaption style={{paddingRight:"40px", color:"white", textDecoration:"none"}}>@Carl Morgan</figcaption></a>
+              <img src={simone} className="img-fluid" alt="avatar" style={{ minHeight: "450px"}}/>
+              <a target="_blank" href="https://www.linkedin.com/in/simoneo/"><figcaption style={{color:"white", textDecoration:"none"}}>@Simone O'Callaghan</figcaption></a>
             </Tilt>
           </Col>
         </Row>
