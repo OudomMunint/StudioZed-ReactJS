d71501abe88c9b513df45d12af20d3804df68a91
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2023-03-18 12:59:37 +1100

    New handler for form submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,53 +1,60 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-const ContactForm = () => {
-  const [formStatus, setFormStatus] = React.useState('Send')
-  const onSubmit = (e) => {
-    e.preventDefault()
-    setFormStatus('Submitting...')
-    const { name, email, message } = e.target.elements
-    let conFom = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    }
-    console.log(conFom)
-  }
-  return (
-    <div class="formFlex">
-    <div class="form">
-    <div class="top-bar">
-          <span></span>
-          <span></span>
-          <span></span>
-    </div>
-      <div class="title">Get in touch!</div>
+import React, { useState } from 'react';
+import axios from 'axios';
 
-      <form onSubmit={onSubmit}>
-        <div class="input-container ic1" >
-          <label className="form-label" class="Formplaceholder" htmlFor="name">
-            Name
-          </label>
-          <input className="form-control" class="input" type="text" id="name" required />
+function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = { name, email, message };
+    try {
+      const response = await axios.post('/send-email', data);
+      console.log('Form submission successful:', response.data);
+      // clear form inputs
+      setName('');
+      setEmail('');
+      setMessage('');
+    } catch (error) {
+      console.error('Form submission error:', error);
+    }
+  };
+
+  return (
+    <div className="formFlex">
+      <div className="form">
+        <div className="top-bar">
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
-        <div class="input-container ic2" >
-          <label className="form-label" class="Formplaceholder" htmlFor="email">
-            Email
-          </label>
-          <input className="form-control" class="input" type="email" id="email" required />
-        </div>
-        <div class="input-container ic2">
-          <label className="form-label" class="Formplaceholder" htmlFor="message">
-            Message
-          </label>
-          <textarea className="form-control" class="input" id="message" required />
-        </div>
-        <button className="btn btn-danger" class="submit" type="submit">
-          {formStatus}
-        </button>
-      </form>
+        <div className="title">Get in touch!</div>
+        <form onSubmit={handleSubmit}>
+          <div className="input-container ic1" >
+            <label className="form-label" htmlFor="name">
+              
+            </label>
+            <input className="form-control" class="input" type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' required />
+          </div>
+          <div className="input-container ic2" >
+            <label className="form-label" htmlFor="email">
+              
+            </label>
+            <input className="form-control" class="input" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' required />
+          </div>
+          <div className="input-container ic2">
+            <label className="form-label" htmlFor="message">
+              
+            </label>
+            <textarea className="form-control" class="input from-textarea" id="message" value={message} onChange={(e) => setMessage(e.target.value)} placeholder='Message' required />
+          </div>
+          <button className="btn btn-danger submit" type="submit" style={{ position: 'relative', marginTop: '68px' }}>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 export default ContactForm
