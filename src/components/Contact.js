86f9df68f74b0d91eba23d576b2fa3d268a47fdc
OudomMunint <oudommunint@gmail.com>
2023-08-14 19:57:15 +1000

    New form + Netlify handler

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,9 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
 
 function ContactForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = { name, email, message };
-    try {
-      const response = await axios.post('/send-email', data);
-      console.log('Form submission successful:', response.data);
-      // clear form inputs
-      setName('');
-      setEmail('');
-      setMessage('');
-    } catch (error) {
-      console.error('Form submission error:', error);
-      console.log('Error caught');
-    }
-  };
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   return (
     <div className="formFlex">
@@ -31,26 +14,40 @@ function ContactForm() {
           <span></span>
         </div>
         <div className="title">Get in touch!</div>
-        <form onSubmit={handleSubmit} data-netlify="true">
-          <div className="input-container ic1" >
-            <label className="form-label" htmlFor="name">
-              
-            </label>
-            <input className="form-control" class="input" type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' required />
+        <form name="contact" method="POST" data-netlify="true" onSubmit={(e) => {
+            e.preventDefault();
+            alert("Thank you for reaching out! I will get back to you soon.");
+            setName("");
+            setEmail("");
+            setMessage("");
+          }}>
+
+          <input type="hidden" name="form-name" value="contact" />
+
+          {/* Name */}
+          <div className="input-container ic1">
+            <label className="form-label" htmlFor="name"></label>
+            <input className="form-control" class="input" type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)}
+              placeholder="Name"
+              required />
           </div>
-          <div className="input-container ic2" >
-            <label className="form-label" htmlFor="email">
-              
-            </label>
-            <input className="form-control" class="input" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' required />
-          </div>
+
+          {/* Email */}
           <div className="input-container ic2">
-            <label className="form-label" htmlFor="message">
-              
-            </label>
-            <textarea className="form-control" class="input from-textarea" id="message" value={message} onChange={(e) => setMessage(e.target.value)} placeholder='Message' required />
+            <label className="form-label" htmlFor="email"></label>
+            <input className="form-control" class="input" type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              required/>
           </div>
-          <button className="btn btn-danger submit" type="submit" style={{ position: 'relative', marginTop: '68px' }}>
+
+          {/* Message */}
+          <div className="input-container ic2">
+            <label className="form-label" htmlFor="message"></label>
+            <textarea className="form-control" class="input from-textarea" id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)}
+            placeholder="Message"
+            required />
+          </div>
+          <button className="btn btn-danger submit" type="submit" style={{ position: "relative", marginTop: "68px" }}>
             Submit
           </button>
         </form>
@@ -58,4 +55,5 @@ function ContactForm() {
     </div>
   );
 }
-export default ContactForm
+
+export default ContactForm;
