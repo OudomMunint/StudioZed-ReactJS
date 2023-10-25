ba8f76aab180f497dbb082713d619dfba02c8ec9
OudomMunint <oudommunint@gmail.com>
2023-10-25 19:59:15 +1100

    Web Vitals issue. #31, misc ui change to preloader

--- src/index.js
+++ src/index.js
@@ -15,4 +15,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // Web vitals report generation here
-reportWebVitals();
+reportWebVitals(console.log);
