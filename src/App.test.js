3556cdb5ab5d76be9b66cc89c2fd83246f690c78
OudomMunint <oudommunint@gmail.com>
2024-08-17 16:50:00 +1000

    #84 - Added more tests.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,57 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ContactForm from './components/Contact';
+import reportWebVitals from "./reportWebVitals";
+import About from './components/About/About';
 
-test('renders learn react link', () => {
+// Test home page content
+test('Test home page content', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement1 = screen.getByText(/Welcome!/i);
+  const linkElement2 = screen.getByText(/We are/i);
+  const linkElements3 = screen.getAllByText(/Studio Zed/i);
+
+  expect(linkElement1).toBeInTheDocument();
+  expect(linkElement2).toBeInTheDocument();
+  expect(linkElements3.length).toBeGreaterThan(0); // If atleast 1 match.
+});
+
+// Test contact form
+test('Find contact form', async () => {
+  render(<ContactForm />);
+  const formTitle = await screen.findByText(/Get in touch!/i);
+  const formFieldName = await screen.findByPlaceholderText(/Name/i);
+  const formFieldEmail = await screen.findByPlaceholderText(/Email/i);
+  const formFieldMessage = await screen.findByPlaceholderText(/Message/i);
+
+  expect(formTitle).toBeInTheDocument();
+  expect(formFieldName).toBeInTheDocument();
+  expect(formFieldEmail).toBeInTheDocument();
+  expect(formFieldMessage).toBeInTheDocument();
+});
+
+// Test form submit button
+test('Find form submit button', () => {
+  render(<ContactForm />);
+  const submitButton = screen.getByRole('button', { name: /Submit/i });
+  expect(submitButton).toBeInTheDocument();
+});
+
+// Test web vitals
+test('Test web vitals', () => {
+  reportWebVitals(metric => {
+    console.log('Web Vitals metric:', metric);
+    expect(metric).toBeDefined();
+    expect(metric.name).toBeTruthy();
+  });
+});
+
+// Test if about page is rendered
+test('Test if about page is rendered', () => {
+  render(<About />);
+  const aboutPage = About();
+  if (aboutPage != null) {
+    console.log("About page is rendered");
+  }
+  expect(aboutPage).not.toBeNull();
 });
