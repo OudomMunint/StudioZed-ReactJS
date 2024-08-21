98aa51ddd29e549ee586b317a993cb2de76a363f
OudomMunint <oudommunint@gmail.com>
2024-08-21 19:33:46 +1000

    Code clean up

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -27,39 +27,12 @@ function About() {
       <Particle />
       <Container>
       <Home2/>
-        {/* <Row style={{ justifyContent: "center", padding: "10px" }}>
-          <Col
-            md={7}
-            style={{
-              justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
-            }}
-          >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
-            </h1>
-            <Aboutcard />
-          </Col>
-          <Col
-            md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
-            className="about-img"
-          >
-            <img src={laptopImg} alt="about" className="img-fluid" />
-          </Col>
-        </Row> */}
         <h1 className="project-heading">
           Who we <strong className="purple">work with </strong>
         </h1>
         <p>(This we will change to the logos of those we worked with)</p>
 
         <Clients />
-
-        {/* <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
-        </h1>
-        <Toolstack /> */}
 
         {/* <Github /> */}
       </Container>
