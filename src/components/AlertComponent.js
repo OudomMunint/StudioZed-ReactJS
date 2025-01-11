19e302e19ef23c6cda15600a3690cb690fed9d1e
OudomMunint <oudommunint@gmail.com>
2025-01-12 03:05:12 +1100

    Refactor - accept message prop, streamline alert display logic & handle closing of dialog locally. #96

--- src/components/AlertComponent.js
+++ src/components/AlertComponent.js
@@ -1,39 +1,32 @@
 import React, { useState, useEffect, useCallback } from "react";
 
-function AlertComponent() {
+const hideAlert = () => {
+  const alertElement = document.querySelector(".alert-container");
+  if (alertElement) {
+    alertElement.classList.add("hidden");
+  }
+};
 
-  const [animateAlert, setAnimateAlert] = useState(false);
-  
-  const hideAlert = () => {
-    const alertElement = document.querySelector(".alert-container");
-    if (alertElement) {
-      alertElement.classList.add("hidden");
-    }
-  };
-
+const AlertComponent = ({ message, animateAlert }) => {
   return (
-    <AlertComponent>
-      {/* Alert Message */}
-      <div className={`alert-container ${animateAlert ? "" : "hidden"}`} id="alert">
-            <p style={{ margin: "0" }}>
-              This website serves as a showcase of my development work. The source code is hosted on GitHub and deployed via my personal Netlify account.
-              Therefore, all emails sent through this form will be directed to me. For inquiries related to
-              <strong className="purple"> Studio Zed</strong>, please contact
-              <a href="https://www.newcastle.edu.au/profile/simone-ocallaghan"> Dr. Simone O'Callaghan</a>.
-            </p>
-            <button onClick={hideAlert}
-              style={{
-                backgroundColor: "transparent",
-                border: "none",
-                color: "#721c24",
-                cursor: "pointer",
-                float: "right",
-              }}>
-              &#x2715;
-            </button>
-          </div>
-    </AlertComponent>
+    <div className={`alert-container ${animateAlert ? "" : "hidden"}`} id="alert">
+      <p style={{ margin: "0" }}>
+        {message}
+      </p>
+      <button
+        onClick={hideAlert}
+        style={{
+          backgroundColor: "transparent",
+          border: "none",
+          color: "#721c24",
+          cursor: "pointer",
+          float: "right",
+        }}
+      >
+        &#x2715;
+      </button>
+    </div>
   );
-}
+};
 
 export default AlertComponent;
