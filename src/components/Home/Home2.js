da2e2ec9119ae75be4b911ebd5e3cd1d12efe8f9
OudomMunint <oudommunint@gmail.com>
2025-02-14 00:52:38 +1100

    #61 - Dark mode work for about page.

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -94,19 +94,19 @@ function Home2() {
               <a target="_blank " href="https://www.instagram.com/zookraft/?hl=en">
                 <b className="white" style={{fontSize:"30px"}}>
                   {" "}
-                  <AiOutlineInstagram style={{color:"black"}}/>
+                  <AiOutlineInstagram className="fa-icon"/>
                 </b>
               </a>
               &nbsp;or&nbsp;
               <a target="_blank " href="https://www.instagram.com/zookraft/?hl=en">
-                <b className="white" style={{fontSize:"30px"}}><AiOutlineLinkedin style={{color:"black"}}/></b>
+                <b className="white" style={{fontSize:"30px"}}><AiOutlineLinkedin className="fa-icon"/></b>
                 </a>
             </p>
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
               <img src={carl} className="img-fluid" alt="avatar" style={{ minHeight: "450px"}}/>
-              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/carl-morgan-7066863b/"><figcaption style={{paddingRight:"40px", color:"black", textDecoration:"none"}}>@Carl Morgan</figcaption></a>
+              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/carl-morgan-7066863b/"><figcaption className="fa-icon">@Carl Morgan</figcaption></a>
             </Tilt>
           </Col>
         </Row>
@@ -140,14 +140,14 @@ function Home2() {
               </a> */}
               &nbsp;
               <a target="_blank " href="https://www.linkedin.com/in/simoneo/">
-                <b className="white" style={{fontSize:"30px"}}><AiOutlineInstagram style={{color:"black"}} /></b>
+                <b className="white" style={{fontSize:"30px"}}><AiOutlineInstagram className="fa-icon" /></b>
                 </a>
             </p>
           </Col>
           <Col md={4} className="myAvtar2">
             <Tilt>
               <img src={simone} className="img-fluid" alt="avatar" style={{ minHeight: "450px"}}/>
-              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/simoneo/"><figcaption style={{color:"black", textDecoration:"none"}}>@Simone O'Callaghan</figcaption></a>
+              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/simoneo/"><figcaption className="fa-icon">@Simone O'Callaghan</figcaption></a>
             </Tilt>
           </Col>
         </Row>
