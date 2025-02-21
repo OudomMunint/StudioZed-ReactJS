0171329813673fbcc2c50f3559523877f01c6ce4
OudomMunint <oudommunint@gmail.com>
2025-02-21 22:57:24 +1100

    Stopped using triggerjs, implemented effect with intersectio observer.

--- src/components/About/ScrollTrigger.js
+++ src/components/About/ScrollTrigger.js
@@ -3,36 +3,58 @@ import Card from "react-bootstrap/Card";
 import { useEffect } from "react";
 
 function ScrollTrigger() {
+
+  useEffect(() => {
+    let currentIntersectedElement = null;
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // Reset previous element
+          // if (currentIntersectedElement && currentIntersectedElement !== entry.target) {
+          //   currentIntersectedElement.style.opacity = 0.1;
+          // }
+          entry.target.style.opacity = 1;
+          currentIntersectedElement = entry.target;
+        } else {
+          entry.target.style.opacity = 0.1;
+          if (currentIntersectedElement === entry.target) {
+            currentIntersectedElement = null;
+          }
+        }
+      });
+    }, {
+      threshold: 1,
+      rootMargin: "-150px",
+    });
+
+    document.querySelectorAll('.triggerSpan').forEach((element) => {
+      element.style.opacity = 0.1;
+      observer.observe(element);
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <Card className="quote-card-view">
       <Card.Body>
-      <div id="aboutMe" className="body">
+        <div id="aboutMe" className="body">
           <div className="triggerDiv" tg-name="_" tg-ref="container" tg-from="1" tg-to="8" tg-step="1">
             <section tg-name="_" tg-map="1,2,3,4,5,6,7,8,9,10: 1">
-              <span className="triggerSpan" tg-name="opacity" tg-filter="2!" tg-follow="container"> 
-                I am an experienced software engineer based in Newcastle, New South Wales Australia, open to work remotely.
+              <span className="triggerSpan" tg-name="opacity" tg-filter="2!" tg-follow="container">
+              A University Of Newcastle Student Led Design Studio.
               </span>
               <br />
               <span className="triggerSpan" tg-name="opacity" tg-filter="3!" tg-follow="container">
-                An IT graduate from the University of Newcastle,
-                i have a passion for web and app development.
-              </span>
+              We work together with communities, governments, universities and select start-up businesses
+              </span> <br />
               <span className="triggerSpan" tg-name="opacity" tg-filter="4!" tg-follow="container">
-                I'm currently working at <a className="pds" href="https://www.pdsglobal.com/" target="_blank" style={{color: "white"}}>Productivity Developement Solutions</a> as a software engineer.
+              We work on large scale projects from illustration and graphic design through to campaigns and creative consultancy.
               </span>
               <br />
               <span className="triggerSpan" tg-name="opacity" tg-filter="5!" tg-follow="container">
-                I love to design dashboards, UI effects, animations as well as making
-                dynamic, creative and intuitive user experiences & Interface.
-              </span>
-              <span className="triggerSpan" tg-name="opacity" tg-filter="6!" tg-follow="container">
-                I love computer graphics, I build and optimize graphically insane game
-                environments and implement the latest technologies Like DLSS, FSR and
-                much more.
-              </span>
-              <span className="triggerSpan" tg-name="opacity" tg-filter="7!" tg-follow="container">
-                I am also interested in PC hardware, Micro-Architectures and the latest
-                technologies in computing.
+              Feel free to get in touch for an obligation free consultation. And everything from Aye! to Zed!
               </span>
             </section>
           </div>
