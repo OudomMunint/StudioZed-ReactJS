bf69786adb00dca27103df8a1e2d51e78968d01c
OudomMunint <oudommunint@gmail.com>
2025-02-22 15:27:19 +1100

    Show different sections based on screen width

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -5,6 +5,7 @@ import Tilt from "react-parallax-tilt";
 import carl from "../../Assets/carl.svg"
 import simone from "../../Assets/simone.svg"
 import ScrollTrigger from "../About/ScrollTrigger";
+import { useMediaQuery } from "react-responsive";
 
 import {
   // AiFillGithub,
@@ -19,52 +20,80 @@ import {
 // import myImg from "../../Assets/avatar.svg";
 
 function Home2() {
+  const isLargeScreen = useMediaQuery({ query: "(min-width: 768px)" });
+  
   return (
     <Container fluid className="home-about-section" id="about">
       <Container>
         <Row>
-          {/* <Col md={8} className="home-about-description">
-          
-            <h1 className="home2H1" style={{ fontSize: "2.2em", width: "500px", textAlign: "left"  }}>
-              A University Of Newcastle <span className="purple"> Student Led </span> Design Studio.
-            </h1>
-            <p className="home-about-body" style={{width: "500px", textAlign: "left", fontSize: "2.2em"  }}>
-              <br />
-              <br />We work together with
-              <i>
-                <b className="purple">  communities, governments, universities and select start-up businesses </b>
-              </i>
-              <br />
-              <br />
-              We work on large scale projects from&nbsp;
-              <i>
-                <b className="purple">llustration and graphic design </b> through to{" "}
-                <b className="purple">
-                campaigns and creative consultancy.
-                </b>
-              </i>
-              <br />
-              <br />
-              Feel free to get in touch for an <b className="purple">obligation free consultation.</b> And everything from
-              <i>
-                <b className="purple">
-                  {" "}
-                  Aye!
-                </b>
-              </i>
-              &nbsp;to
-              <i>
-                <b className="purple"> Zed!</b>
-              </i>
-            </p>
-          </Col>
+        {!isLargeScreen && (
+            <Col md={8} className="home-about-description">
+              <h1
+                className="home2H1"
+                style={{
+                  fontSize: "2.2em",
+                  width: "500px",
+                  textAlign: "left",
+                }}
+              >
+                A University Of Newcastle{" "}
+                <span className="purple"> Student Led </span> Design Studio.
+              </h1>
+              <p
+                className="home-about-body"
+                style={{
+                  width: "500px",
+                  textAlign: "left",
+                  fontSize: "2.2em",
+                }}
+              >
+                <br />
+                <br />
+                We work together with
+                <i>
+                  <b className="purple">
+                    {" "}
+                    communities, governments, universities and select start-up
+                    businesses{" "}
+                  </b>
+                </i>
+                <br />
+                <br />
+                We work on large scale projects from&nbsp;
+                <i>
+                  <b className="purple">llustration and graphic design </b>{" "}
+                  through to{" "}
+                  <b className="purple">campaigns and creative consultancy.</b>
+                </i>
+                <br />
+                <br />
+                Feel free to get in touch for an{" "}
+                <b className="purple">obligation free consultation.</b> And
+                everything from
+                <i>
+                  <b className="purple"> Aye!</b>
+                </i>
+                &nbsp;to
+                <i>
+                  <b className="purple"> Zed!</b>
+                </i>
+              </p>
+            </Col>
+          )}
+
+          {!isLargeScreen && (
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={Student} className="img-fluid" alt="avatar" style={{ minHeight: "450px" }}/>
+              <img
+                src={Student}
+                className="img-fluid"
+                alt="avatar"
+                style={{ minHeight: "450px" }}
+              />
             </Tilt>
-          </Col> */}
-          <ScrollTrigger/>
+          </Col>)}
 
+          {isLargeScreen && <ScrollTrigger className="scroll-trigger" />}
         </Row>
       </Container>
       {/* about end */}
